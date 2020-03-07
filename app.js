de7d6262924748c2804bf83e91abdb2d9cc8fe55
Joao <jvsadesign@gmail.com>
2020-03-07 11:08:10 -0300

    adjust some things

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 });
 app.use('/task', taskRouter);
 app.use('/user', userRouter);
+const host = '0.0.0.0';
 const port = process.env.APP_PORT || 3000;
 const server = app.listen(process.env.APP_PORT || 4000, host);
 console.log(`Server is running at ${server.address().port}`);
