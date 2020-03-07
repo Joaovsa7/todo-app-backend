17e65e6cc4b57ab39a758d3d53a5ec81d78451a2
Joao <jvsadesign@gmail.com>
2020-03-07 11:05:53 -0300

    set initial route

--- app.js
+++ app.js
@@ -14,4 +14,5 @@ app.use('/task', taskRouter);
 app.use('/user', userRouter);
 const host = '0.0.0.0';
 const port = process.env.APP_PORT || 3000;
-const server = app.listen(process.env.APP_PORT || 4000, host, () => console.log(`Server is running at ${server.address().port}`));
+const server = app.listen(process.env.APP_PORT || 4000, 0.0.0.0);
+console.log(`Server is running at ${server.address().port}`);
