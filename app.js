181c1411bcea18a9bf1d5b867f51c6d8f91f989f
Joao <jvsadesign@gmail.com>
2020-03-07 10:47:54 -0300

    updating some things

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('./config/database');
-
 const app = express();
 const taskRouter = require('./routes/task');
 const userRouter = require('./routes/user');
-const PORT = 4000;
 
 app.use(express.json());
 app.use(cors('*'));
 app.use('/task', taskRouter);
 app.use('/user', userRouter);
-
-mongoose.connection.on('connected', () => app.listen(PORT, () => console.log('Server and mongoDB is running')));
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.listen(process.env.APP_PORT, () => console.log('Server is running'));
--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 const mongoUrl = process.env.MONGO_CONNECTION_STRING;
 mongoose.connect(mongoUrl);
 mongoose.Promise = global.Promise;
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = mongoose;
