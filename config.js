d2e31b723f91297d8f16465081486d29dcee32d5
Joao <jvsadesign@gmail.com>
2020-03-07 10:16:03 -0300

    removing sensitive code

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-const user = 'joaovsa'
-const password = 'WOhQQKDAfuEQYro'
-
 module.exports = {
   getConnection: () =>
-		`mongodb+srv://${user}:${password}@cluster0-rsntu.mongodb.net/test?retryWrites=true&w=majority`
-}
+    `${process.env.MONGO_CONNECTION_STRING}`
+};
