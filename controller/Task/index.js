28d1ca9dd9178c9360e9d80e5c6744eb3b843f85
Joao <jvsadesign@gmail.com>
2020-03-07 22:48:01 -0300

    semantic message return - ap

--- controller/Task/index.js
+++ controller/Task/index.js
@@ -25,7 +25,7 @@ module.exports = {
 
     try {
       const taskCreated = await TasksModel.create({ ...taskObj });
-      res.send({ task: taskCreated, success: `Task ${taskCreated.title} was created.` });
+      res.send({ ...taskCreated, success: `Task ${taskCreated.title} was created.` });
     } catch (e) {
       res.send({
         error: `${e}`,
